22c871aa7f112f4a950922a1e85be387b2ac0a42
akshaymehtani <akshay.mehtani@gmail.com>
2015-09-02 19:40:06 -0400

    Final Commit
    All changes done make it work according to description on webpage.
    Ready to submit once tested on ECEmachine!

--- step1/src/Micro.java
+++ step1/src/Micro.java
@@ -14,9 +14,7 @@ public class Micro {
         while (tokenScan.hasNextLine())
         {
             String line = tokenScan.nextLine();
-            //System.out.println(line);
             int splitPoint = line.indexOf('=');
-            //System.out.println(""+splitPoint);
             String typeName = line.substring(0, splitPoint);//this is token type name
             int key = Integer.parseInt((line.substring(splitPoint + 1)));//this is int value of that type
             tokenNameLookup.put(key, typeName);
@@ -29,58 +27,43 @@ public class Micro {
     {
         tokenNameLookup.clear();
         formLookup();//forming the lookup table for token type
-        String[] fileNames = {"fibonacci", "loop", "nested", "sqrt"};
-        int index = 0;
+        
+        //get file name
+        String currentFile = args[0];
 
-        while (index < 4) {
-            //get file name
-            String currentFile = fileNames[index] + ".micro";
-            //get string stream for test
-            CharStream text =  new ANTLRFileStream(currentFile);
-            MicroLexer lexer = new MicroLexer(text);
+        //get string stream for test
+        CharStream text =  new ANTLRFileStream(currentFile);
+        MicroLexer lexer = new MicroLexer(text);
 
-            //setup an output fie for current file, for diffing later
-            String outputFileName = fileNames[index] + "Out.txt";
-            PrintWriter out = new PrintWriter(new File(outputFileName));
+        while (true)
+        {
+            Token token = lexer.nextToken();
+            if (token.getType() == MicroLexer.EOF) break;
 
 
-            while (true)
+            String tokenType = tokenNameLookup.get(token.getType());
+            String tokenValue = token.getText();
+
+            if (!tokenType.equals("FORMAT") && !tokenType.equals("STRINGLITERAL") && !tokenType.equals("COMMENT"))//surpress irrelevat tokens
             {
-
-
-                Token token = lexer.nextToken();
-                if (token.getType() == MicroLexer.EOF) break;
-
-
-                String tokenType = tokenNameLookup.get(token.getType());
-                String tokenValue = token.getText();
-                //boolean supress = tokenValue.contains("\n") || tokenValue.contains("\r") || tokenValue.contains("--");
-
-                if (!tokenType.equals("FORMAT") && !tokenType.equals("STRINGLITERAL") && !tokenType.equals("COMMENT"))//surpress irrelevat tokens
+                if (tokenValue.contains(" "))
                 {
-                    if (tokenValue.contains(" "))
-                    {
-                        out.write("Token Type: " + tokenType + "\n");
-                        out.write("Value: \" \" \n");
-                    }
-                    else
-                    {
-                        out.write("Token Type: " + tokenType + "\n");
-                        out.write("Value: " + tokenValue + "\n");
-                    }
-
+                    System.out.print("Token Type: " + tokenType + "\n");
+                    System.out.print("Value: \" \" \n");
                 }
-                if(tokenType.equals("STRINGLITERAL")) 
+                else
                 {
-                    out.write("Token Type: " + tokenType + "\n");
-                    out.write("Value: " + tokenValue + "\n");
+                    System.out.print("Token Type: " + tokenType + "\n");
+                    System.out.print("Value: " + tokenValue + "\n");
                 }
 
             }
+            if(tokenType.equals("STRINGLITERAL")) 
+            {
+                System.out.print("Token Type: " + tokenType + "\n");
+                System.out.print("Value: " + tokenValue + "\n");
+            }
 
-            out.close();
-
-            index++;
         }
     }
 }

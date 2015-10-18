6220511a0eb8e711b4a48c665e4c201caeae4774
akshaymehtani <akshay.mehtani@gmail.com>
2015-10-18 18:08:41 -0400

    OpNode Code generated
    Simple and Call nodes remaining

--- step4/src/opNode.java
+++ step4/src/opNode.java
@@ -22,17 +22,31 @@ public class opNode extends ASTnode {
         String thisOperation = getValue();
 
         //case 1: simplest code form
-        boolean bool1 = leftNode.getType().equals("VALUE");
-        boolean bool2 = rightNode.getType().equals("VALUE");
-        boolean bool3 = leftNode.getType().equals("VAR");
-        boolean bool4 = rightNode.getType().equals("VAR");
-
-        String leftValue = leftNode.getValue();
-        String rightValue = rightNode.getValue();
+        boolean bool1 = leftNode.getType().equals("INT");
+        boolean bool2 = rightNode.getType().equals("INT");
+        boolean bool3 = leftNode.getType().equals("FLOAT");
+        boolean bool4 = rightNode.getType().equals("FLOAT");
+        boolean bool5 = leftNode.getType().equals("VAR");
+        boolean bool6 = rightNode.getType().equals("VAR");
 
         if (bool1 && bool2) //two number
         {
-            if(leftValue.contains("."))//left is a float
+            if(thisOperation.equals("+")) {
+                code = "ADDI";
+            }
+
+            if(thisOperation.equals("-")) {
+                code = "SUBI";
+            }
+
+            if(thisOperation.equals("*")) {
+                code = "MULTI";
+            }
+
+            if(thisOperation.equals("/")) {
+                code = "DIVI";
+            }
+            /*if(leftValue.contains("."))//left is a float
             {
                 if(rightValue.contains("."))
                 {
@@ -47,21 +61,264 @@ public class opNode extends ASTnode {
                 }
             }
             //since it's just a number
-            code = result;
+            code = result;*/
         }
-
-        if (bool3 && bool4) //two variables
+        else if (bool3 && bool4) //two variables
         {
+            if(thisOperation.equals("+")) {
+                code = "ADDF";
+            }
 
+            if(thisOperation.equals("-")) {
+                code = "SUBF";
+            }
+
+            if(thisOperation.equals("*")) {
+                code = "MULTF";
+            }
+
+            if(thisOperation.equals("/")) {
+                code = "DIVF";
+            }
         }
-
-        if( (bool1&&bool4) || (bool2&&bool3))
+        else if((bool1&&bool4) || (bool2&&bool3))
         {
+            if(thisOperation.equals("+")) {
+                code = "ADDF";
+            }
 
+            if(thisOperation.equals("-")) {
+                code = "SUBF";
+            }
+
+            if(thisOperation.equals("*")) {
+                code = "MULTF";
+            }
+
+            if(thisOperation.equals("/")) {
+                code = "DIVF";
+            }
+        }
+        else if(bool5&&bool6) {
+            String leftValue = leftNode.getValue();
+            String rightValue = rightNode.getValue();
+
+            String lefttype = getVarType(leftValue);
+            String righttype = getVarType(righttValue);
+
+            if(lefttype.equals("INT") && righttype.equals("INT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDI";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBI";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTI";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVI";
+                }        
+            }
+
+            if(lefttype.equals("FLOAT") && righttype.equals("FLOAT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDF";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBF";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTF";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVF";
+                }        
+            }
+
+            if((lefttype.equals("FLOAT") && righttype.equals("INT")) || (lefttype.equals("INT") && righttype.equals("FLOAT"))) {
+                if(thisOperation.equals("+")) {
+                code = "ADDF";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBF";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTF";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVF";
+                }        
+            }   
         }
 
+        else if(bool1&&bool6) {
+            String rightValue = rightNode.getValue();
+            String righttype = getVarType(righttValue);
 
+            if(righttype.equals("INT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDI";
+                }
 
+                if(thisOperation.equals("-")) {
+                    code = "SUBI";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTI";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVI";
+                }
+            }
+            else if(righttype.equals("FLOAT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDF";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBF";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTF";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVF";
+                }
+            }
+        }
+        else if(bool3&&bool6) {
+            String rightValue = rightNode.getValue();
+            String righttype = getVarType(righttValue);
+
+            if(righttype.equals("INT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDF";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBF";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTF";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVF";
+                }
+            }
+            else if(righttype.equals("FLOAT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDF";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBF";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTF";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVF";
+                }
+            }
+        }
+
+        else if(bool5&&bool2) {
+            String leftValue = leftNode.getValue();
+            String lefttype = getVarType(righttValue);
+
+            if(lefttype.equals("INT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDI";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBI";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTI";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVI";
+                }
+            }
+            else if(lefttype.equals("FLOAT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDF";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBF";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTF";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVF";
+                }
+            }
+        }
+
+        else if(bool5&&bool4) {
+            String leftValue = leftNode.getValue();
+            String lefttype = getVarType(righttValue);
+
+            if(lefttype.equals("INT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDF";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBF";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTF";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVF";
+                }
+            }
+            else if(lefttype.equals("FLOAT")) {
+                if(thisOperation.equals("+")) {
+                code = "ADDF";
+                }
+
+                if(thisOperation.equals("-")) {
+                    code = "SUBF";
+                }
+
+                if(thisOperation.equals("*")) {
+                    code = "MULTF";
+                }
+
+                if(thisOperation.equals("/")) {
+                    code = "DIVF";
+                }
+            }
+        }
     }
 
 
